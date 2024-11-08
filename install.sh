b29ed4cc54bc223c566a2d253b8d5ad3505fa1ef
yuki-kimura-safie <yuki.kimura@safie.jp>
2024-11-08 11:16:27 +0900

    Fix

--- install.sh
+++ install.sh
@@ -120,7 +120,7 @@ fi
 # Make Synbolic Links
 for f in .??*
 do
-    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".git"* ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 
     ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
